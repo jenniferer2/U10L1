2b70b63afe5175335b42cdcb20ea16396e10c69f
BT_1E10_09 <dariusj0714@gmail.com>
2022-03-30 12:13:30 -0400

    Initial commit

---
+++ src/Summing.java
@@ -0,0 +1,20 @@
+public class Summing
+{
+    public static void main(String[] args)
+    {
+        System.out.println(sum(6));  // should print 21
+        System.out.println(sum(10)); // should print 55
+    }
+
+    public static int sum(int n)
+    {
+
+
+        if (n == 0) {
+            return 0;
+        }
+        else {
+            return n + sum(n-1);
+        }
+    }
+}
